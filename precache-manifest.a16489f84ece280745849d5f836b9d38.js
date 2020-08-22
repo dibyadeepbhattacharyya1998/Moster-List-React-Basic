db36973d4ba3c28bf5b7a0e69a99c84c3724c0a9
Dibyadeep Bhattacharya <dibyadeepbhattacharyya1998@gmail.com>
2020-08-22 22:33:45 +0530

    Updates

---
+++ precache-manifest.a16489f84ece280745849d5f836b9d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a70f10c07a4581b9ba82c26852891c",
+    "url": "/Moster-List-React-Basic/index.html"
+  },
+  {
+    "revision": "79e6bc8d98f260eaf1aa",
+    "url": "/Moster-List-React-Basic/static/css/main.377ff61f.chunk.css"
+  },
+  {
+    "revision": "73c74a9bb63118e4d3d0",
+    "url": "/Moster-List-React-Basic/static/js/2.e3a2b14b.chunk.js"
+  },
+  {
+    "revision": "79e6bc8d98f260eaf1aa",
+    "url": "/Moster-List-React-Basic/static/js/main.731b7dd7.chunk.js"
+  },
+  {
+    "revision": "e4dc727579a325bd6fe5",
+    "url": "/Moster-List-React-Basic/static/js/runtime~main.a3bd235a.js"
+  }
+]);
